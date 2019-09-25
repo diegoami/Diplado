8972980deb0c1f9b013d0fb8d66371941b84bf7c
Diego Amicabile <diego.amicabile@gmail.com>
2019-09-25 22:22:45 +0200

    chore: directory for master module

--- source/module_info.py
+++ source/module_info.py
@@ -1,4 +1,5 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
-export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Diplomacy_1.7/"
-#export_dir = "../"
+#export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master/"
+export_dir = "/Users/diegoamicabile/Library/ApplicationSupport/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master"
+
