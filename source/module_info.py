34481dfdb343b32ce6eeb524b73986a5b464ba61
Diego Amicabile <diego.amicabile@gmail.com>
2019-09-26 07:05:30 +0200

    chore: restore the original structure (must commit the compiled files as well)

--- source/module_info.py
+++ source/module_info.py
@@ -1,5 +1,6 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 #export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master/"
-export_dir = "/Users/diegoamicabile/Library/ApplicationSupport/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master"
+#export_dir = "/Users/diegoamicabile/Library/ApplicationSupport/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master"
 
+export_dir = ".."
