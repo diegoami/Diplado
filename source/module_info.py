f6c03188298f313ac4026be1954dc0b60fbec5e9
Diego Amicabile <diego.amicabile@amicabile.com>
2019-09-28 12:28:56 +0200

    fix: variables not correct

--- source/module_info.py
+++ source/module_info.py
@@ -3,4 +3,4 @@
 #export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master/"
 #export_dir = "/Users/diegoamicabile/Library/ApplicationSupport/Steam/steamapps/common/MountBlade Warband/Modules/Diplado_master"
 
-export_dir = ".."
+export_dir = "../"
